0a70218b73660943b1c79fcadfa5936847c6922a
Benoit Gloria <gloria_b@epita.fr>
2019-08-28 12:51:27 +0200

    access to sibling

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,40 @@ pub fn get_grandparent(node: &Node) -> Option<Rc<Node>> {
     get_parent(node).and_then(|parent| get_parent(&*parent))
 }
 
+/// Return the sibling of the node
+/// ```
+/// use red_black_tree::{Node, COLOR};
+/// use std::rc::Rc;
+///
+/// let left_left = Node::new(0, None, None, COLOR::BLACK);
+/// let right_right = Node::new(40, None, None, COLOR::BLACK);
+/// let left = Node::new(10, Some(&left_left), None, COLOR::RED);
+/// let right = Node::new(30, None, Some(&right_right), COLOR::RED);
+/// let root = Node::new(20, Some(&left), Some(&right), COLOR::BLACK);
+///
+/// assert!(Rc::ptr_eq(&right, &red_black_tree::get_sibling(Rc::clone(&left)).unwrap()));
+/// assert!(Rc::ptr_eq(&left, &red_black_tree::get_sibling(Rc::clone(&right)).unwrap()));
+/// assert!(red_black_tree::get_sibling(Rc::clone(&left_left)).is_none());
+/// assert!(red_black_tree::get_sibling(Rc::clone(&right_right)).is_none());
+/// assert!(red_black_tree::get_sibling(Rc::clone(&root)).is_none());
+/// ```
+pub fn get_sibling(node: Rc<Node>) -> Option<Rc<Node>> {
+    get_parent(&*node).and_then(|parent| {
+        parent
+            .left
+            .borrow()
+            .as_ref()
+            .and(parent.right.borrow().as_ref())
+            .and_then(|right| {
+                if Rc::ptr_eq(&node, &right) {
+                    Some(Rc::clone(parent.left.borrow().as_ref().unwrap()))
+                } else {
+                    Some(Rc::clone(right))
+                }
+            })
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
